46776853e738f518bfe471193f2e655b8a227078
ParkMiHyun <mihyunpark24@gmail.com>
2021-04-15 14:46:36 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    #플라스크 앱 생성 시 blueprint적용하기
+    from views import main
+    app.register_blueprint(main.bp)
+
+    return app
