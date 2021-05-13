670b1ed02de1c11ed98152ed1755bd0a32537bbd
smwu <smwu@smwuui-MacBookAir.local>
2021-05-13 22:34:40 +0900

    refact:최적화 방법 변경

--- service/SvmTrainingService.py
+++ service/SvmTrainingService.py
@@ -1,5 +1,7 @@
 from sklearn import svm, metrics
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas as pd
 import joblib
@@ -7,7 +9,13 @@ import pickle
 
 #data load
 data_set = pd.read_excel("/Users/smwu/Desktop/data_set.xlsx")
-
+'''
+#google drive spreadsheet load
+url = "https://docs.google.com/spreadsheets/d/1CGp9VJsO5NSY57AJMM7wmYcfEZpL0lyn9srYlIQVbqo/view?usp=sharing"
+file_id = url.split('/')[-2]
+dwn_url = 'https://drive.google.com/uc?id=' + file_id
+data_set = pd.read_excel(dwn_url)
+'''
 #행, 열 추출 방법1
 #x1 = data_set.iloc[:, 0]
 #y1 = data_set.iloc[:, 1]
@@ -16,7 +24,7 @@ data_set = pd.read_excel("/Users/smwu/Desktop/data_set.xlsx")
 data = data_set.to_numpy() #pandas객체를 numpy화
 x=[]
 y=[]
-for index, d in enumerate(data):
+for index, d in enumerate(data)
     x.append(d[0])
     y.append(d[1])
 
@@ -34,28 +42,16 @@ result = svm.predict(model_test) #model_test는 벡터화된 x_test
 print(metrics.accuracy_score(y_test, result))
 
 #최적화
-gamma_list = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
-acc = 0
-for index,gamma_data in enumerate(gamma_list): # enumerate를 사용한 이유는 enumerate에 익숙해지기 위하여
-    # 소수점 테스트
-    svm = svm.SVC(C=1,gamma=gamma_data)
-    svm.fit(model_train,y_train)
-    result=svm.predict(model_test)
-    score = metrics.accuracy_score(y_test, result)
-    if acc < score:
-        acc = score
-        svm_best = svm
-    # 정수 테스트
-    svm = svm.SVC(C=1,gamma=gamma_data*10)
-    svm.fit(model_train,y_train)
-    result = svm.predict(model_test)
-    score = metrics.accuracy_score(y_test, result)
-    if acc < score:
-        acc = score
-        svm_best = svm
-
+pipe_svc = make_pipeline(StandardScaler(), svm.SVC())
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+param_grid = [
+    {'svc__C' : param_range, 'svc__kernel':['linear']},
+    {'svc__C' : param_range, 'svc__gamma':param_range, 'svc__kernel':['rbf']},
+]
+gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid, scoring='accuracy', cv=10, n_jobs=2)
+gs = gs.fit(model_train, y_train)
 #최적화 결과
-result = svm_best.predict(model_test) #model_test는 벡터화된 x_test
+result = gs.predict(model_test) #model_test는 벡터화된 x_test
 print(metrics.accuracy_score(y_test, result))
 
 #학습된 모델 저장
