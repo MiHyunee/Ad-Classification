51738b6e3b4bfe0dcf3cf6cfb957008e4c95e125
ParkMiHyun <mihyunpark24@gmail.com>
2021-05-18 14:51:11 +0900

    fix: ocr request exception 해결

--- service/ocrService.py
+++ service/ocrService.py
@@ -11,24 +11,33 @@ def ocrTest(blogArray):
         last = i.getImagePath().getLastImage()
         sticker = i.getSticker()
 
-        if(first != None):
-            response = requests.get(first)
-            img = Image.open(io.BytesIO(response.content))
-            text = pytesseract.image_to_string(img, config='--psm 1', lang='kor')
-            # , config='--psm 1'
-            i.setFirstOCR(text.replace("\n", ' '))
+        try:
+            if(first != None):
+                response = requests.get(first)
+                img = Image.open(io.BytesIO(response.content))
+                text = pytesseract.image_to_string(img, config='--psm 1', lang='kor')
+                # , config='--psm 1'
+                i.setFirstOCR(text.replace("\n", ' '))
+        except:
+            i.setLastOCR("")
 
-        if(last != None):
-            response = requests.get(last)
-            img = Image.open(io.BytesIO(response.content))
-            text = pytesseract.image_to_string(img, config='--psm 1', lang='kor')
-            i.setLastOCR(text.replace("\n", ' '))
+        try:
+            if(last != None):
+                response = requests.get(last)
+                img = Image.open(io.BytesIO(response.content))
+                text = pytesseract.image_to_string(img, config='--psm 1', lang='kor')
+                i.setLastOCR(text.replace("\n", ' '))
+        except:
+            i.setLastOCR("")
 
-        if(sticker != None):
-            response = requests.get(sticker)
-            img = Image.open(io.BytesIO(response.content))
-            text = pytesseract.image_to_string(img, config='--psm 1', lang='kor')
-            i.setStickerOCR(text.replace("\n", ' '))
-            print("{0} stickerOCR: {1}".format(k, i.getStickerOCR()))
+        try:
+            if(sticker != None):
+                response = requests.get(sticker)
+                img = Image.open(io.BytesIO(response.content))
+                text = pytesseract.image_to_string(img, config='--psm 1', lang='kor')
+                i.setStickerOCR(text.replace("\n", ' '))
+                print("{0} stickerOCR: {1}".format(k, i.getStickerOCR()))
+        except:
+            i.setStickerOCR("")
         k=k+1
     return blogArray
