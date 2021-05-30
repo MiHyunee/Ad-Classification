afca11a94395d22f9917b0498c30869381c94e59
smwu <smwu@smwuui-MacBookAir.local>
2021-05-30 09:41:58 +0900

    refact:영역추출 ocrService에 적용

--- service/OcrProcessingService.py
+++ service/OcrProcessingService.py
@@ -1,17 +1,13 @@
 import cv2
-from pytesseract import *
-import pytesseract
 import numpy as np
 
-def bwization():
+def bwization(thres):
     img = cv2.imread("/Users/smwu/Desktop/sample_image/bw5.png")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    r, dst = cv2.threshold(gray,230, 255, cv2.THRESH_BINARY_INV)
+    r, dst = cv2.threshold(gray,thres, 255, cv2.THRESH_BINARY_INV)
+    #cv2.imwrite('binary.jpg', dst) #테스트 코드
 
-    cv2.imwrite('binary.jpg', dst)
-
-    text = pytesseract.image_to_string(dst, config='--psm 3', lang='kor')
-    print(text)
+    return dst
 
 def contour(rgb, ec, rec_w, rec_h):
     #rgb = cv2.imread("/Users/smwu/Desktop/sample_image/bw3.png") #local test
@@ -20,7 +16,7 @@ def contour(rgb, ec, rec_w, rec_h):
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (ec, ec))
     grad = cv2.morphologyEx(gray, cv2.MORPH_GRADIENT, kernel)
     _, bw = cv2.threshold(grad, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    cv2.imwrite('bw.jpg', bw)
+    #cv2.imwrite('bw.jpg', bw)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (rec_w, rec_h))
     connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
     contours, _ = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -29,7 +25,7 @@ def contour(rgb, ec, rec_w, rec_h):
     for idx in range(len(contours)):
         x, y, w, h = cv2.boundingRect(contours[idx])
         cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
-        cv2.imwrite('mask.jpg',mask)
+        #cv2.imwrite('mask.jpg',mask)
         r = float(cv2.countNonZero(mask[y:y + h, x:x + w])) / (w * h)
         if r > 0.5 and w > 15 and h > 9:
             img.append(rgb[y:y + h, x:x + w])
@@ -37,3 +33,4 @@ def contour(rgb, ec, rec_w, rec_h):
     #cv2.imwrite('divide.jpg', rgb)  # 텍스트 영역추출 테스트 코드용
 
     return img
+
--- service/OcrService.py
+++ service/OcrService.py
@@ -5,36 +5,51 @@ import pytesseract
 import requests
 from service.OcrProcessingService import contour
 import cv2
+import numpy as np
 
 def ocrTest(blogArray):
     k=0
+    a = 0
     for i in blogArray:
         last = i.getImagePath().getLastImage()
         sticker = i.getSticker()
-
+        text=""
         try:
             if(last != None):
                 response = requests.get(last)
                 img = Image.open(io.BytesIO(response.content))
-                text=""
+                #PIL image cv2 타입으로 변환
+                np_img=np.array(img)
+                cv_img = cv2.cvtColor(np_img, cv2.COLOR_RGB2BGR)
+                print(last)
                 #processing
-                imgArray = contour(img, 3, 9, 3)
+                imgArray = contour(cv_img, 3, 9, 3)
                 if(len(imgArray) > 2):
-                    contour(img, 10, 35, 9)
-                for image in imgArray:
-                    t = pytesseract.image_to_string(image, config='--psm 1', lang='kor')
-                    text += t
-                i.setLastOCR(text)
+                    contour(cv_img, 10, 35, 9)
+                print(len(imgArray))
+                if(len(imgArray)<11):
+                    for fraction in imgArray:
+                        #테스트 코드
+                        '''
+                        a=a+1
+                        print(a)
+                        cv2.imwrite('f'+str(a)+'.jpg',fraction)
+                        '''
+                        t = pytesseract.image_to_string(fraction, config='--psm 4', lang='kor')
+                        text += t
+                i.setLastOCR(text.replace('\n', ' '))
         except:
             i.setLastOCR("")
+        print(text.replace('\n', ' ')) #테스트 코드
 
         try:
             if(sticker != None):
                 response = requests.get(sticker)
                 img = Image.open(io.BytesIO(response.content))
-                text = pytesseract.image_to_string(img, config='--psm 1', lang='kor')
+                text = pytesseract.image_to_string(img, config='--psm 4', lang='kor')
                 i.setStickerOCR(text.replace("\n", ' '))
         except:
             i.setStickerOCR("")
+
         k=k+1
     return blogArray
