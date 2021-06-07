b5d2f46c190becbacae7b136e1e995380add5366
ParkMiHyun <mihyunpark24@gmail.com>
2021-06-08 06:16:06 +0900

    feat: ad표기 동적생성

--- chromeExtension/js/popup.js
+++ chromeExtension/js/popup.js
@@ -1,17 +1,45 @@
 
 //check 버튼 누르면
 function waitMessage(){
-  $("#message").html( '<center>'+
-                      '<h3>Getting...</h3>'+
-                      '</center>'
-                    );
+  $("#message").html( '<center>'+ '<h3>Getting...</h3>'+ '</center>');
 }
 
+function doneMessage() {
+    $("#message").html('<center>' + '<h3>Done</h3>' + '</center>');
+}
+
+function inject(data) {
+    chrome.tabs.executeScript(null, {
+        code: 'var newdata = "' + data + '";'
+    }, function() {
+        chrome.tabs.executeScript(null, {
+            code: `
+            var predict = newdata.split(",");
+            var element = Array.from(document.getElementsByClassName("writer_info"));
+            for (var i in element) {
+                if(predict[i]=="Ad") {
+                    var newDiv = document.createElement('span');
+                    newDiv.style.backgroundColor = 'red';
+                    newDiv.style.height = "20px";
+                    newDiv.style.width = "40px";
+                    newDiv.style.borderRadius = "3px"
+                    newDiv.style.marginLeft = "10px"
+                    var newText = document.createTextNode(predict[i]);
+                    newDiv.appendChild(newText);
+                    newDiv.style.color = "white";
+                    newDiv.style.fontSize = "14px";
+                    newDiv.style.textAlign = "center";
+                    element[i].appendChild(newDiv);
+                }
+            }
+            `
+        });
+    });
+}
 
 //문서가 준비되면 매개변수로 넣은 콜백함수를 실행
 $(document).ready(function(){
 
-  //checking whether spam or not spam
   $("#getData").click(function () {
       //waitMessage출력
       waitMessage();
@@ -29,11 +57,12 @@ $(document).ready(function(){
                 contentType: "application/json; charset=utf-8",
                 data: JSON.stringify(result),
                 success: function (data) {
-                    var response = data;
-                    output = data['response'] + 'is_ad' + data['isAd'] + 'index' + data['index'];
-                    chrome.runtime.sendMessage({type: "isStatus", count: data});
-                    data = null;
-                    }
+                    inject(data.results);
+                    doneMessage();
+                },
+                error: function (error) {
+                    console.log("error");
+                }
                 })
           }
       )
