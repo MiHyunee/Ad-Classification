95d09f3b658edb952e2cf50670e00f62e3838e15
smwu <smwu@smwuui-MacBookAir.local>
2021-08-25 23:58:30 +0900

    refact: 에러 핸들러 추가

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from service.ReportService import error_handle
 
 
 def create_app():
@@ -7,5 +8,5 @@ def create_app():
     #플라스크 앱 생성 시 blueprint적용하기
     from views import main
     app.register_blueprint(main.bp)
-
+    error_handle(app)
     return app
--- service/ReportService.py
+++ service/ReportService.py
@@ -30,7 +30,7 @@ def reporting(url):
     wait(driver,3).until(EC.element_to_be_clickable((By.ID, 'log.login'))).click()
     if(driver.current_url != "https://www.naver.com/"):
         driver.quit()
-        return error_response("로그인에 실패했습니다", "LoginError", 1001)
+        raise LoginError
 
     # 신고페이지로 이동
     driver.get('https://help.naver.com/support/contents/contents.help?serviceNo=964&categoryNo=2826')
@@ -43,17 +43,30 @@ def reporting(url):
 
     if(check_alert(driver)):
         driver.quit()
-        return error_response("네이버 블로그만 가능합니다. URL을 다시 확인해주세요", "UrlError",1002)
+        raise UrlError
 
     driver.find_element_by_id('r1').click()
     driver.find_element_by_id('_btn_add_url').click()
 
     if(check_alert(driver)):
         driver.quit()
-        return error_response("등록에 실패했습니다.", "AddError", 1003)
+        raise AddError
 
     #driver.find_element_by_id('btn btn_confrm').click()
 
+def error_handle(app):
+
+    @app.errorhandler(LoginError)
+    def handler_error(e):
+        return error_response("로그인에 실패했습니다","LoginError"),1001
+
+    @app.errorhandler(UrlError)
+    def handle_error(e):
+        return error_response("네이버 블로그만 가능합니다. URL을 다시 확인해주세요", "UrlError"),1002
+
+    @app.errorhandler(AddError)
+    def handler_error(e):
+        return error_response("등록에 실패했습니다.", "AddError"), 1003
 
 def error_response(user_error_message, dev_error_message, status_code):
     response = {
@@ -63,3 +76,9 @@ def error_response(user_error_message, dev_error_message, status_code):
                 }
     return response
 
+class LoginError(Exception): #로그인 에러 정의
+    pass
+class UrlError(Exception): #url 에러 정의
+    pass
+class AddError(Exception): # 등록 실패 에러 정
+    pass
