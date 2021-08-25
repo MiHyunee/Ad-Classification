66cd4f0b284f2c750c650da8ac20dfd64a9bc8a6
smwu <smwu@smwuui-MacBookAir.local>
2021-08-25 23:16:28 +0900

    feat: 에러핸들링

--- service/ReportService.py
+++ service/ReportService.py
@@ -14,6 +14,7 @@ def check_alert(driver): #경고창 확인하는 함수
         return False
 
 def reporting(url):
+    url = "dd"
     driver = webdriver.Chrome()
 
     #네이버 로그인
@@ -29,8 +30,9 @@ def reporting(url):
 
     wait(driver,3).until(EC.element_to_be_clickable((By.ID, 'log.login'))).click()
     if(driver.current_url != "https://www.naver.com/"):
-        print('로그인 실패')
         driver.quit()
+        return error_response("로그인에 실패했습니다", "LoginError", 1001)
+        #raise LoginError
 
     # 신고페이지로 이동
     driver.get('https://help.naver.com/support/contents/contents.help?serviceNo=964&categoryNo=2826')
@@ -43,15 +45,23 @@ def reporting(url):
 
     if(check_alert(driver)):
         driver.quit()
-        print('url 이상')  # url실패 사실 프론트에 알려주기
+        return error_response("네이버 블로그만 가능합니다. URL을 다시 확인해주세요", "UrlError",1002)
 
     driver.find_element_by_id('r1').click()
     driver.find_element_by_id('_btn_add_url').click()
 
     if(check_alert(driver)):
         driver.quit()
-        print('등록 실패')
+        return error_response("등록에 실패했습니다.", "AddError", 1003)
 
-    print('good')
     #driver.find_element_by_id('btn btn_confrm').click()
+
+
+def error_response(user_error_message, dev_error_message, status_code):
+    response = {
+                    "user_error_message" : user_error_message,
+                    "dev_error_message" : dev_error_message,
+                    "status_code": status_code
+                }
+    return response
 
